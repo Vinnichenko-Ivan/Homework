ec1f4f8d9219990b099dfa0a3a33c4b22bb9c309
Vinnichenko-Ivan <33024117+Vinnichenko-Ivan@users.noreply.github.com>
2020-02-17 09:45:07 +0700

    Create main2.0.cpp

---
+++ Lab18/main2.0.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<fstream>
+#include<ctime>
+#include<cmath>
+#include<cstdlib>
+using namespace std;
+int main(){
+    
+    srand(time(0));
+    ifstream fin("input");
+    ofstream fout("output");
+    long long int n=100000,m=0;
+    float x,y,z,a=2,b=3,c=4;
+    int answer[5];
+    for(int i=0;i<n;i++){
+
+        x=a*2*(float)rand()/RAND_MAX-a;
+        y=b*2*(float)rand()/RAND_MAX-b;
+        z=c*2*(float)rand()/RAND_MAX-c;
+        if(x*x/a/a+y*y/b/b+z*z/c/c<=1){
+            m++;
+        }
+        if(i==999){
+            answer[0]=m;
+        }
+        if(i==4999){
+            answer[1]=m;
+        }
+        if(i==14999){
+            answer[2]=m;
+        }
+        if(i==49999){
+            answer[3]=m;
+        }
+        if(i==99999){
+            answer[4]=m;
+        }
+    }
+    cout<<4*m/n;
+    fout<<1000<<"\t"<<5000<<"\t"<<15000<<"\t"<<50000<<"\t"<<100000<<endl;
+    for(int i=0;i<5;i++){
+        fout<<answer[i]<<"\t";
+    }
+    fin.close();
+    fout.close();
+   
+    return 0;
+}
